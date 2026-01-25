d4ea03b70e4bb255f05cb1237e712c3004e1ebeb
huzaifakhan42732-creator <huzaifakhan42732@gmail.com>
2026-01-25 20:01:14 +0500

    Connect to mongoDb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { DB_NAME } from './constant.js';
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
 
-const app = express();
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+connectDB();
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+// const app = express();
+// dotenv.config();
+
+// (async ()=>{
+//     try {
+// await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+//         console.log('Connected to MongoDB');
+//         app.on("error", (err) => {
+//             console.log("Error in app:", err);
+//             throw err;});
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+        
+//         });
+//     } catch (error) {
+//         console.log('Error:', error);
+//         throw error;
+//     }
+// })();
